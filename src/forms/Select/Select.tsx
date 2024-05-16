4fdc1d29a9a1432f82dfa2941cce9648173e318c
okoli-ryan <okoliryan50@gmail.com>
2024-05-16 17:46:47 +0100

    light change on select

--- src/forms/Select/Select.tsx
+++ src/forms/Select/Select.tsx
@@ -22,7 +22,7 @@ export default function Select({ label, name, selectProps, selectClassName = "",
 			<select
 				onChange={(e) => form.setFieldValue(name as string, e.target.value)}
 				className={twMerge(
-					"w-full !h-[40.6px] md:!h-14 outline-none py-0 md:py-4 px-4 border-light-blue border border-solid  rounded-md md:rounded-[10px]  xl:text-base lg:text-sm",
+					"w-full !h-[40.6px] md:!h-14 outline-none py-0 md:py-4 px-4 border-light-blue border bg-light-2 border-solid  rounded-md md:rounded-[10px]  xl:text-base lg:text-sm",
 					selectClassName
 				)}
 				{...selectProps}>
