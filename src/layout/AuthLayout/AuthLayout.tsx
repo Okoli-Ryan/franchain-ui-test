fbf81b365efc8b2dca6ccf871372e22d20ee0704
Ugo Okoli <ookoli@systemspecs.com.ng>
2024-05-16 22:11:27 +0100

    scrolling issue

--- src/layout/AuthLayout/AuthLayout.tsx
+++ src/layout/AuthLayout/AuthLayout.tsx
@@ -7,13 +7,13 @@ import AuthImage from '@images/auth-image.svg';
 
 export default function AuthLayout() {
 	return (
-		<div className="w-screen justify-center flex min-h-screen h-full">
+		<div className="w-screen justify-center flex min-h-screen">
 			<div className="w-full justify-center flex">
 				<div className="max-w-[1300px] w-full grid grid-cols-1 lg:grid-cols-2 z-10  relative">
 					<div className="h-[110px] flex items-center px-10 absolute">
 						<img src={Logo} alt="logo" className="left-10 top-5 hidden lg:block md:h-14" />
 					</div>
-					<div className="lg:flex justify-center items-center flex-col h-full px-[60px] hidden py-[290px]">
+					<div className="lg:flex justify-center items-center flex-col h-full px-[60px] hidden py-[120px]">
 						<img src={AuthImage} alt="auth-image" className="max-w-[427px] w-full h-auto" />
 						<h4 className="mt-[34px] text-center w-full font-medium leading-[52px] text-[40px]">
 							Royalty Collections, simplified. It's next level!
